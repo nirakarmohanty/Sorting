ceed24d7b8c664617c6c46b00f74b262682a6483
NIRAKAR <NIRAKAR@NIRAKAR-PC>
2014-11-23 19:25:19 +0530

    Updated code

--- CORE_JAVA_PRACTICE_PROGRAM/src/com/nirakar/ds/StringReverseUsingStack.java
+++ CORE_JAVA_PRACTICE_PROGRAM/src/com/nirakar/ds/StringReverseUsingStack.java
@@ -1,10 +1,26 @@
 package com.nirakar.ds;
 
-public class StringReverseUsingStack {
+public class StringReverseUsingStack extends Stack{
+	
+
+	public StringReverseUsingStack(int size) {
+		super(size);
+		
+	}
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		StringReverseUsingStack strRev= new StringReverseUsingStack(5);
+		strRev.push(1);
+		strRev.push(2);
+		strRev.push(3);
+		strRev.push(4);
+		strRev.push(5);
+		String str="";
+		for(int i=0;i<5;i++){
+			int stackVar = strRev.pop();
+			str = str+stackVar;
+		}
+		System.out.println(str);
 	}
 
 }
